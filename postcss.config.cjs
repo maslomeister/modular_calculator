6026f2c66af101416fa62f1aaa3ea126fa0d8441
Nikita Maslov <maslomeister@gmail.com>
2023-03-11 21:58:47 +0400

    minified css for prod

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -3,5 +3,6 @@ module.exports = {
     "postcss-import": {},
     tailwindcss: { config: "./tailwind.config.cjs" },
     autoprefixer: {},
+    ...(process.env.NODE_ENV === "production" ? { cssnano: {} } : {}),
   },
 };
